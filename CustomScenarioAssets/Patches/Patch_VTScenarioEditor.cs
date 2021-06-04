74200468c27f4edd5653e6af0b60ee592175336b
THE-GREAT-OVERLORD-OF-ALL-CHEESE <oscarjamesbristow@gmail.com>
2021-06-04 16:26:25 +0100

    1.1 Removed the ability to upload to the workshop
    Avoid softlocking stock users by not letting modified missions on the workshop

---
+++ CustomScenarioAssets/Patches/Patch_VTScenarioEditor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using UnityEngine;
+
+[HarmonyPatch(typeof(VTScenarioEditor), "UploadToSteamWorkshop")]
+class Patch_VTScenarioEditor_UploadToSteamWorkshop
+{
+    [HarmonyPrefix]
+    static bool Prefix(VTScenarioEditor __instance)
+    {
+        __instance.confirmDialogue.DisplayConfirmation("I'm sorry dave", "I can't let you do that yet. The game doesn't have any fail safes for if it loads a scenario with an object it doesn't know and it softlocks peoples games. This will probably be fixed in an update.", null, null);
+        return false;
+    }
+}
